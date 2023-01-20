8d2a24ed6aaf0639c120ed4e3cd8245d228dfb23
FENRlR <limitwolf@gmail.com>
2023-01-20 11:19:14 -0500

    first prototype

--- main.py
+++ main.py
@@ -1,51 +1,36 @@
-#import
+import shutil as sl
 
 
-#var 정리
-#var log = null;
-#
-#var imgPattern = /^.*\.(?:jpg|jpeg|png|gif|webp)$/i;
+swit = input("Choose a task \nzip = 0, unzip = 1 \n")
+print(f"received : {swit}")
 
-#swit = input("Choose a task \nzip = 0, unzip = 1 \n")
-#print(f"received : {swit}")
-
-path = input("Specify the file path : \n")
-print(f"received : {path}")
-
-file = open(path,"rb").read().hex()
-
-print(file)
-
-
-
-"""
+#"""
 
 if(swit==1):#unzip
     # - Path
     path = input("Specify the file path : \n")
     print(f"received : {path}")
 
-    file = open(path,"rb").read().hex()
-
-    #print(file)
-
-
-
-
+    file = open(path, "rb").read().hex()
+    new = "504b0304" + file.split("ffd9504b0304", 2)[1]
+    result = open("./sample/result.zip", "wb").write(bytes.fromhex(new))  # bytes-like object is required, not 'str'
+    sl.unpack_archive("./sample/result.zip", "./sample/output")
 
 elif(swit==0):#zip
     # - Path
-    path = input("Specify the file path : \n")
-    print(f"received : {path}")
+    imgpath = input("Specify img file path : \n")
+    print(f"received : {imgpath}")
+
+    zippath = input("Specify zip file path : \n")
+    print(f"received : {zippath}")
+
+    new = open(imgpath, "rb").read().hex() + open(zippath, "rb").read().hex()
+
+    result = open("./sample/zipresult.jpg", "wb").write(bytes.fromhex(new))
+    sl.unpack_archive("./sample/result.zip", "./sample")
 
 
-
-"""
-
-
-
-#def upzip():
-
+#"""
 
 
 
