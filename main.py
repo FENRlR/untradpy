ed9e4df1d33c105c5bf8901428bbb2aa13b63e04
FENRlR <limitwolf@gmail.com>
2023-01-20 01:56:59 -0500

    init

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-#-
+#import
+
+
+#var 정리
+#var log = null;
+#
+#var imgPattern = /^.*\.(?:jpg|jpeg|png|gif|webp)$/i;
+
+#- Paste the path
+path = input("Specify the file path : \n")
+print(f"received : {path}")
+
+
+
+#def upzip():
+
+
+
+
+
+
+
+
 
