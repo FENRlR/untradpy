779edfbfa50e2ebdb80b958d20ed8eb8683d78c9
FENRlR <limitwolf@gmail.com>
2023-01-20 11:50:58 -0500

    variable type declared

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import shutil as sl
 
 
-swit = input("Choose a task \nzip = 0, unzip = 1 \n")
+swit = int(input("Choose a task \nzip = 0, unzip = 1 \n"))
 print(f"received : {swit}")
 
-#"""
 
 if(swit==1):#unzip
     # - Path
     path = input("Specify the file path : \n")
     print(f"received : {path}")
+
 
     file = open(path, "rb").read().hex()
     new = "504b0304" + file.split("ffd9504b0304", 2)[1]
@@ -27,8 +27,6 @@ elif(swit==0):#zip
     new = open(imgpath, "rb").read().hex() + open(zippath, "rb").read().hex()
 
     result = open("./sample/zipresult.jpg", "wb").write(bytes.fromhex(new))
-
-#"""
 
 
 
