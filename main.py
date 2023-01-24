8f3c04425e289f6cf6c8c7036222ed5c350927f3
FENRlR <limitwolf@gmail.com>
2023-01-24 02:15:50 -0500

    some changes to input

--- main.py
+++ main.py
@@ -14,41 +14,65 @@ swit = int(input("- Choose a task \nDecompress = 1, Compress = 0 \n"))
 print(f"received : {swit}")
 
 if (swit == 1):
-    # - Unzip : jpg to zip result
+    # - Unzip : jpg -> zip -> result
     path = input("- Specify the jpg file path : \n")
-    if open(path, "rb").read() == null:
-        print("The path does not exist")
-    else:
+
+    try:
+        open(path, "rb").read()
         print(f"received : {path}")
+    except:
+        print("The path does not exist")
 
     file = open(path, "rb").read().hex()
     new = "504b0304" + file.split("ffd9504b0304", 2)[1]  # TODO : error handling
     uznm = getfname(path)
+
     open("./sample/" + uznm + ".zip", "wb").write(bytes.fromhex(new))  # bytes-like object is required, not 'str'
     print(f"DONE - Check '{uznm}.zip' in './sample'")
 
     exswit = int(input("Do you want to unzip it?\nYES = 1, No = 0\n"))
-    if exswit == 1:
-        sl.unpack_archive("./sample/" + uznm + ".zip", "./sample/output/" + uznm)
-        print(f"DONE - Check the result in './sample/output/{uznm}'")
+
+    while(exswit != 1  or exswit != 0 ):
+        if exswit == 1:
+            sl.unpack_archive("./sample/" + uznm + ".zip", "./sample/output/" + uznm)
+            print(f"DONE - Check the result in './sample/output/{uznm}'")
+        elif exswit == 0:
+            break
+        else:
+            continue
 
 elif (swit == 0):
-    # - Zip : zip to jpg
-    zippath = input("Specify zip file path : \n")
+    # - Zip : file -> zip -> jpg
+    # jpg|jpeg|png|gif|webp
+    zippath = input("- Specify the file path : \n")
 
-    if open(zippath, "rb").read() == null:
-        print("The path does not exist")
-    else:
+    try:
+        open(zippath, "rb").read()
         print(f"received : {zippath}")
+    except:
+        print("The path does not exist")
+
+    sp = zippath.split("/")
+    nsp = sp.pop()
+    ssp = "/".join(map(str, sp))
+    nzip = "./sample/temp/" + nsp.split(".")[0] + ".zip"
+
+    sl.make_archive("./sample/temp/" + nsp.split(".")[0], "zip", ssp, nsp)
 
     imgpath = input("- Specify the jpg file path : \n")
-    if imgpath == null or open(imgpath, "rb").read() == null:
+    try:
+        open(imgpath, "rb").read()
+        print(f"received : {imgpath}")
+    except:
         imgpath = "./default.jpg"
         print("Image not found - proceed with default image")
-    else:
-        print(f"received : {imgpath}")
 
-    new = open(imgpath, "rb").read().hex() + open(zippath, "rb").read().hex()
+    new = open(imgpath, "rb").read().hex() + open(nzip, "rb").read().hex()
     znm = getfname(zippath)
     open("./sample/" + znm + ".jpg", "wb").write(bytes.fromhex(new))
+    try:
+        sl.rmtree("./sample/temp/")
+    except:
+        pass
+
     print(f"DONE - Check '{znm}.jpg' in './sample'")
